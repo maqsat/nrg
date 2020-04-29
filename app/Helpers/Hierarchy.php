305ff6abaf13c0d6457418415ba906e2d805eea6
Maqsat <batyrqul.maqsat@gmail.com>
2020-04-29 17:58:09 +0600

    fix sq

--- app/Helpers/Hierarchy.php
+++ app/Helpers/Hierarchy.php
@@ -282,7 +282,7 @@ class Hierarchy {
                                 $package = Package::find($innerItem->package_id);
                                 $sum = $package->pv*20/100*env('COURSE');
                                 echo $item->user_id."<br>";
-                                Balance::changeBalance($item->user_id,$sum,'quickstart_bonus',$innerItem->id,1,$package->id,$item->status_id,$package->pv);
+                                Balance::changeBalance($item->user_id,$sum,'quickstart_bonus',$innerItem->user_id,1,$package->id,$item->status_id,$package->pv);
                             }
                         }
 
